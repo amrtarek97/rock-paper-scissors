c55e847dcf1c85ca795c70e7cebc87091d5b4d43
amrtarek97 <121955814+amrtarek97@users.noreply.github.com>
2024-03-11 16:05:41 +0200

    create a random computer choice function

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function playRound(playerSelection, computerSelection) {
+function getComputerChoice() {
+    const choice = ['rock', 'paper', 'scissors'];
+    const compChoice = choice[(Math.floor(Math.random()*3))];
+    return compChoice;
+}
+
+// console.log(getComputerChoice());
+
+
+
+
+
+
+
+// function playRound(playerSelection, computerSelection) {
     
-  }
+//   }
   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+//   const playerSelection = "rock";
+//   const computerSelection = getComputerChoice();
+//   console.log(playRound(playerSelection, computerSelection));
   
