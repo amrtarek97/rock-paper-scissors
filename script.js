8bcac2be17bd72e3d0282ab486c5b43d7c72bd1e
amrtarek97 <121955814+amrtarek97@users.noreply.github.com>
2024-03-11 16:15:49 +0200

    Create a play round function

--- script.js
+++ script.js
@@ -12,11 +12,26 @@ function getComputerChoice() {
 
 
 
-// function playRound(playerSelection, computerSelection) {
-    
-//   }
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper'){
+        return "You lose! rock loses to paper";
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors'){
+        return "You lose! paper loses to scissors";
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock'){
+        return "You lose! scissors loses to rock";
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors'){
+        return "You win! rock beats scissors";
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock'){
+        return "You win! paper beats rock";
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper'){
+        return "You win! scissors beats paper";
+    } else {
+        return "Draw!";
+    }
+}
   
-//   const playerSelection = "rock";
-//   const computerSelection = getComputerChoice();
-//   console.log(playRound(playerSelection, computerSelection));
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(computerSelection);
+  console.log(playRound(playerSelection, computerSelection));
   
