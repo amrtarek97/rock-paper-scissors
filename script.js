a2703baa24f29ae2b438c2a74a29202b10d2b62b
amrtarek97 <121955814+amrtarek97@users.noreply.github.com>
2024-04-21 16:39:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+let playerScore = 0;
+let computerScore = 0;
+
+let computerSelection;
+let playerSelection;
+
+
+
 function getComputerChoice() {
     const choice = ['rock', 'paper', 'scissors'];
     const compChoice = choice[(Math.floor(Math.random()*3))];
     return compChoice;
 }
 
-// getting player choice from a button
-let rock = document.querySelector('#rock');
-let paper = document.querySelector('#paper');
-let scissors = document.querySelector('#scissors');
-// trying event delegation using the parent (see odin dom lesson assignment)
 
+// getting player choice from a button
+// now that i made the function below i might delete those 3 var
+
+// let rock = document.querySelector('#rock');
+// let paper = document.querySelector('#paper');
+// let scissors = document.querySelector('#scissors');
+
+// trying event delegation using the parent (see odin dom lesson assignment)
+    
 let choices = document.querySelector('.choices');
 choices.addEventListener('click', (e) => {
     let target = e.target;
@@ -33,38 +45,39 @@ choices.addEventListener('click', (e) => {
 
 
 
+
 // end of button choices section
-let playerSelection;
+
 
 // console.log(getComputerChoice());
 
+// console.log(computerSelection);
 
-let playerScore = 0;
-let computerScore = 0;
-
-let computerSelection;
 
 
 function playRound(playerSelection, computerSelection) {
+    
+    // no need for prompt now?
+    // playerSelection = prompt("pick one: rock, paper, scissors: ");
     computerSelection = getComputerChoice();
-    playerSelection = prompt("pick one: rock, paper, scissors: ");
 
-    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper'){
+    if (playerSelection == 'rock' && computerSelection == 'paper'){
         computerScore++;
+        console.log('shebsheb');
         return "You lose! rock loses to paper, player score: "+playerScore+". Computer score: "+computerScore+"." ;
-    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors'){
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors'){
         computerScore++;
         return "You lose! paper loses to scissors, player score: "+playerScore+". Computer score: "+computerScore+".";
-    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock'){
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock'){
         computerScore++;
         return "You lose! scissors loses to rock, player score: "+playerScore+". Computer score: "+computerScore+".";
-    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors'){
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors'){
         playerScore++;
         return "You win! rock beats scissors, player score: "+playerScore+". Computer score: "+computerScore+".";
-    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock'){
+    } else if (playerSelection == 'paper' && computerSelection == 'rock'){
         playerScore++;
         return "You win! paper beats rock, player score: "+playerScore+". Computer score: "+computerScore+".";
-    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper'){
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper'){
         playerScore++;
         return "You win! scissors beats paper, player score: "+playerScore+". Computer score: "+computerScore+".";
     } else {
@@ -72,36 +85,63 @@ function playRound(playerSelection, computerSelection) {
     }
 }
   
+// make button start the game:
+
+let startGameBtn = document.querySelector('#rock');
+
+startGameBtn.addEventListener('click', () => {
+    playRound(playerSelection, computerSelection);
+});
 
 
-function gameFromFive(){
-    while (computerScore<5 && playerScore<5) {
+
+
+// 2 event listeners on same thing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function gameFromFive(){
+//     while (computerScore<5 && playerScore<5) {
     
-        console.log(playRound(playerSelection,computerSelection));
-        if (playerScore==5) {
-            return "You win!"        
-        } else if(computerScore==5){
-            return "You lose!"
-        }
+//         console.log(playRound(playerSelection,computerSelection));
+//         if (playerScore==5) {
+//             return "You win!"        
+//         } else if(computerScore==5){
+//             return "You lose!"
+//         }
         
-    }
-}
+//     }
+// }
 
-function fiveRoundGame (){
-    for (let index = 0; index < 5; index++) {
-       console.log(playRound(playerSelection,computerSelection));
+// function fiveRoundGame (){
+//     for (let index = 0; index < 5; index++) {
+//        console.log(playRound(playerSelection,computerSelection));
 
-    }
-    if(playerScore>computerScore){
-        return "You win!";
-    } else if (computerScore>playerScore) {
-        return "You lose!";
-    } else {
-        return "Draw!";
-    }
-}
+//     }
+//     if(playerScore>computerScore){
+//         return "You win!";
+//     } else if (computerScore>playerScore) {
+//         return "You lose!";
+//     } else {
+//         return "Draw!";
+//     }
+// }
 
 // fiveRoundGame();
 
 // console.log(fiveRoundGame());
+
 
